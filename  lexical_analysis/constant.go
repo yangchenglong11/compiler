880f2b792a9eb01e14ef973e34498157af32ea61
yangchenglong11 <1332456142@qq.com>
2018-01-18 11:13:03 +0800

    add a lexical analysis demo

---  lexical_analysis/constant.go
+++  lexical_analysis/constant.go
@@ -34,13 +34,20 @@ const (
 	Identifier = "TypeIdentifier"
 	Integer    = "TypeInteger"
 	Real       = "TypeReal"
+
+	Number     = 0
+	Letter     = 1
+	Other      = 2
+
+	Space      = 0x20
+	NewLine    = '\n'
 )
 
 var MachineCode = map[string]int{"and": 1, "begin": 2, "bool": 3, "do": 4, "else": 5, "end": 6, "false": 7,
 	"if": 8, "integer": 9, "not": 10, "or": 11, "program": 12, "real": 13, "then": 14, "true": 15, "var": 16,
 	"while": 17, Identifier: 18, Integer: 19, Real: 20,
-	"(": 21, ")": 22, "+": 23, "-": 24, "*": 25, "/": 26, ".": 27, ",": 28, ":": 29, ";": 30, ":=": 31, "=": 32, "<=": 33, "<": 34, "<>": 35,
-	">": 36, ">=": 37}
+	"(": 21, ")": 22, "+": 23, "-": 24, "*": 25, "/": 26, ".": 27, ",": 28, ":": 29, ";": 30, ":=": 31, "=": 32,
+	"<=": 33, "<": 34, "<>": 35, ">": 36, ">=": 37,"err":38,"":39}
 
 type LexicalError struct {
 	Rows     int
---  lexical_analysis/lexical_analysis.go
+++  lexical_analysis/lexical_analysis.go
@@ -31,21 +31,23 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"math"
 	"os"
 )
 
 func what(a byte) int {
 	if (a >= 48) && (a <= 57) {
 		//０-９数字
-		return 0
-	} else if (a >= 97) && (a <= 122) {
+		return Number
+	} else if ((a >= 'a') && (a <= 'z')) || ((a >= 'A') && (a <= 'Z')) {
 		//a-z的字母
-		return 1
+		return Letter
 	} else {
 		//其他的标点符号
-		return 2
+		return Other
 	}
 }
 
@@ -59,13 +61,367 @@ func GetContent(path string) (*bytes.Buffer, error) {
 }
 
 func LexicalAnalysis(path string) (*[]Token, *[]Symble, error) {
-	return nil, nil, nil
+	var (
+		token  []Token
+		symble []Symble
+	)
+	c, err := GetContent(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	for {
+		b, err := CheckByte(c)
+		if err != nil {
+			if err == io.EOF {
+				return &token, &symble, nil
+			}
+		}
+		switch what(b) {
+		case Number:
+			t, num, err := handleNumber(b, c)
+			if err != nil {
+				return nil, nil, err
+			}
+			s := Symble{
+				Number: len(symble) + 1,
+				Kind:   t,
+				Name:   fmt.Sprintf("%s", num),
+			}
+			symble = append(symble, s)
+			to := Token{
+				Label: len(token),
+				Name:  fmt.Sprintf("%s", num),
+				Code:  t,
+				Addr:  len(symble),
+			}
+			token = append(token, to)
+			continue
+		case Letter:
+			t,s,err := handleLetter(b, c)
+			if err != nil {
+				return nil, nil, err
+			}
+			if s == "" {
+				continue
+			}
+			sy := Symble{
+				Number: len(symble) + 1,
+				Kind:   t,
+				Name:   s,
+			}
+			symble = append(symble, sy)
+			to := Token{
+				Label: len(token),
+				Name:  s,
+				Code:  t,
+				Addr:  len(symble),
+			}
+			token = append(token, to)
+			continue
+		case Other:
+			o,s,err := handlerOther(b, c)
+			if err != nil {
+				return nil, nil, err
+			}
+			sy := Symble{
+				Number: len(symble) + 1,
+				Kind:   o,
+				Name:   s,
+			}
+			symble = append(symble, sy)
+			to := Token{
+				Label: len(token),
+				Name:  s,
+				Code:  o,
+				Addr:  len(symble),
+			}
+			token = append(token, to)
+			continue
+		}
+	}
+
+	return nil, nil, err
 }
 
 func CheckByte(f *bytes.Buffer) (byte, error) {
+	cSubTemp, err := f.ReadByte()
+	if err != nil {
+		if err == io.EOF {
+			return Space, err
+		}
+		return 0x0, err
+	}
+
+	t := what(cSubTemp)
+	if t <= Letter {
+		return cSubTemp, nil
+	}
+
+	if cSubTemp == NewLine {
+		rows = rows + 1
+	}
+
+	//判断是否为空格,合并多个空格为一个
+	if isSpace(cSubTemp) {
+		for isSpace(cSubTemp) {
+			cSubTemp, err = f.ReadByte()
+			if err != nil {
+				if err == io.EOF {
+					return Space, err
+				}
+				return 0x0, err
+			}
+		}
+		f.UnreadByte()
+		return Space, nil
+	}
+
+	//判断注释
+	if cSubTemp == '/' {
+		//临时变量，用于检查下一个是否为'/'
+		var cTemp byte
+
+		//再读入一个字符
+		cTemp, _ = f.ReadByte()
+
+		//若为注释一直读入直到换行符，否则退回刚才读入的字符
+		if cTemp == '/' {
+			for cTemp != '\n' {
+				cTemp, err = f.ReadByte()
+			}
+			//遇到注释，在注释结尾返回空格
+			return NewLine, nil
+		} else {
+			//退回刚才读入的字符
+			f.UnreadByte()
+		}
+	}
+	return cSubTemp, nil
+}
+
+func handleNumber(b byte, buf *bytes.Buffer) (int, float64, error) {
+	var (
+		temp = b
+		num  = float64(int(temp) - 48)
+		err  error
+	)
+
+	// continue read
+	temp, err = buf.ReadByte()
+	if err != nil {
+		if err == io.EOF {
+			goto finish
+		}
+		return 0, 0, err
+	}
+
+	// not a number
+	if what(temp) != 0 {
+		// is a real
+		if temp == '.' {
+			// continue read
+			temp, err = buf.ReadByte()
+			if err != nil {
+				if err == io.EOF {
+					goto finish
+				}
+				return 0, 0, err
+			}
+			// calculate num
+			for i := 0; what(temp) == 0; i++ {
+				num = num + float64(int(temp)-48)/(math.Pow10(i))
+				temp, err = buf.ReadByte()
+				if err != nil {
+					if err == io.EOF {
+						goto finish
+					}
+					return 0, 0, err
+				}
+			}
+			goto real
+
+		}
+	}
+
+	// is a Integer
+	for what(temp) == 0 {
+		num = num*10 + float64(int(temp)-48)
+		temp, err = buf.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				goto finish
+			}
+			return 0, 0, err
+		}
+	}
+
+real:
+	fmt.Println("(", MachineCode[Real], ",", num, ")")
+	return MachineCode[Real], num, nil
+finish:
+
+	fmt.Println("(", MachineCode[Integer], ",", num, ")")
+	return MachineCode[Integer], num, nil
 
 }
 
-func isspace(b byte) bool {
-	return b == 0x20
+func handleLetter(b byte, buf *bytes.Buffer) (int,string,error) {
+	var (
+		t []byte
+		temp []byte
+		err error
+	)
+	temp = append(temp,b)
+	switch  {
+	case buf.Len()>=6:
+		t = make([]byte,6)
+		buf.Read(t)
+		temp = append(temp,t...)
+		switch string(temp) {
+		case "integer":
+		case "program":
+			return MachineCode[string(temp)],string(temp),nil
+		default:
+			goto other
+		}
+	case buf.Len()>=4:
+		t = make([]byte,4)
+		buf.Read(t)
+		temp = append(temp,t...)
+		switch string(temp) {
+		case "begin":
+		case "false":
+		case "while":
+			return MachineCode[string(temp)],string(temp),nil
+		default:
+			goto other
+		}
+	case buf.Len()>=3:
+		t = make([]byte,3)
+		buf.Read(t)
+		temp = append(temp,t...)
+		switch string(temp) {
+		case "bool":
+		case "else":
+		case "real":
+		case "then":
+		case "true":
+			return MachineCode[string(temp)],string(temp),nil
+		default:
+			goto other
+		}
+	case buf.Len()>=2:
+		t = make([]byte,2)
+		buf.Read(t)
+		temp = append(temp,t...)
+		switch string(temp) {
+		case "not":
+		case "and":
+		case "end":
+		case "var":
+			return MachineCode[string(temp)],string(temp),nil
+		default:
+			goto other
+		}
+	case buf.Len()>=1:
+		b,_ = buf.ReadByte()
+		temp = append(temp,b)
+		switch string(temp) {
+		case "if":
+		case "or":
+		case "do":
+			return MachineCode[string(temp)],string(temp),nil
+		default:
+			goto other
+		}
+	case buf.Len()>=0:
+		return MachineCode[Identifier], string(temp), nil
+
+	default:
+	}
+other:
+	b, err = buf.ReadByte()
+	if err != nil {
+		if err == io.EOF {
+			return MachineCode[Identifier],string(temp),nil
+		}
+		return 0, "", err
+	}
+	for what(b) == Letter {
+		temp = append(temp,b)
+		b, err = buf.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				return MachineCode[Identifier],string(temp),nil
+			}
+			return 0, "", err
+		}
+	}
+	return MachineCode[Identifier],string(temp),nil
+}
+
+
+func handlerOther(b byte, buf *bytes.Buffer) (int,string,error){
+	var (
+		temp []byte
+	)
+	temp = append(temp,b)
+	switch  {
+	case buf.Len()>=1:
+		b,_ = buf.ReadByte()
+		temp = append(temp,b)
+		switch string(temp) {
+		case ">=":
+		case "<=":
+		case "<>":
+		case ":=":
+			return MachineCode[string(temp)],string(temp),nil
+		default:
+			return MachineCode["err"],"#",nil
+		}
+	case buf.Len()>=0:
+		switch string(temp) {
+		case "+":
+		case "-":
+		case "*":
+		case "/":
+		case ".":
+		case ",":
+		case "(":
+		case ")":
+		case ":":
+		case ";":
+		case "=":
+		case ">":
+		case "<":
+			return MachineCode[string(temp)],string(temp),nil
+		case "":
+			return MachineCode[""],"",nil
+		default:
+			return MachineCode["err"],"#",nil
+		}
+
+	default:
+
+	}
+	return MachineCode["err"],"#",nil
+}
+
+func isSpace(b byte) bool {
+	return b == Space
+}
+
+func main() {
+	t,s,err := LexicalAnalysis("./test.lu")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if t!= nil {
+		fmt.Println(*t)
+	}
+	if s != nil {
+		fmt.Println(*s)
+	}
+
 }
