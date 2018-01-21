4bc2a3a115effe36313c8231c70dfa8505f72d4e
wangriyu <wangriyu1997@gmail.com>
2018-01-21 19:41:25 +0800

    add parser_test #1

--- syntax_analysis/parser_test.go
+++ syntax_analysis/parser_test.go
@@ -10,5 +10,60 @@ import (
 )
 
 func TestParser(t *testing.T) {
+  var (
+    parser Parser
+    /* 文法
+      E -> E + T | T
+      T -> T * F | F
+      F -> P ↑ F | P
+      P -> (E) | i
+    */
+    grammar = map[string]string{// 每一个 k, v 对应表达式 value -> key
+      "E+T": "E",
+      "T": "E",
+      "T*F": "T",
+      "F": "T",
+      "P↑F": "F",
+      "P": "F",
+      "(E)": "P",
+      "i": "P",
+    }
+    relation = [][]string{
+      {Above, Below, Below, Below, Below, Above, Above},
+      {Above, Above, Below, Below, Below, Above, Above},
+      {Above, Above, Below, Below, Below, Above, Above},
+      {Above, Above, Above, "   ", "   ", Above, Above},
+      {Below, Below, Below, Below, Below, Equal, "   "},
+      {Above, Above, Above, "   ", "   ", Above, Above},
+      {Below, Below, Below, Below, Below, "   ", Equal},
+    }
+    Vt = []string{"+", "*", "↑", "i", "(", ")", "#"}
+    Vn = []string{"E", "T", "F", "P"}
+  )
+  parser.Init(grammar, Vt, Vn, relation)
 
+  /* input1 */
+  stack1 := Stack{"#"}
+  input1 := Stack{}
+  for _, v := range []string{"i", "+", "i", "*", "i", "#"} { // 可规约
+    input1.Push(v)
+  }
+  result, err := parser.Analysis(&stack1, &input1)
+  if err != nil {
+    t.Error(err)
+  }
+  if !result {
+    t.Error("规约失败")
+  }
+
+  /* input2 */
+  stack2 := Stack{"#"}
+  input2 := Stack{}
+  for _, v := range []string{"(", "i", "*", "i", ")", "(", ")", "#"} { // 不可规约
+    input2.Push(v)
+  }
+  result, _ = parser.Analysis(&stack2, &input2)
+  if result {
+    t.Error("规约出错")
+  }
 }
